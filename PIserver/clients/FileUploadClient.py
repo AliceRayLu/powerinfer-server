7143b0dc2afc12b368432dc3c947303b12323104
luboheng <211250054@smail.nju.edu.cn>
2025-03-28 15:43:19 +0800

    add no internet connection handle

--- PIserver/clients/FileUploadClient.py
+++ PIserver/clients/FileUploadClient.py
@@ -7,6 +7,7 @@ import requests
 from tqdm import tqdm
 
 from PIserver.utils.files import log_error
+import mmap
 
 class FileUploadClient():
     def __init__(self, model_name: list):
@@ -15,11 +16,15 @@ class FileUploadClient():
         self.mname = model_name[0]
         self.tname = model_name[1]
         
-    def generate_md5(self, file_path):
+    def generate_md5(self, file_path: Path):
+        print(f"Validating file changes {str(file_path)}...")
         md5_hash = hashlib.md5()
+        buffer_size = 1024 * 1024  # 1MB
+        
         with open(file_path, "rb") as f:
-            for chunk in iter(lambda: f.read(4096), b""):
-                md5_hash.update(chunk)
+            with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as mmapped_file:
+                while chunk := mmapped_file.read(buffer_size):
+                    md5_hash.update(chunk)
         return md5_hash.hexdigest()
 
 
@@ -71,46 +76,59 @@ class FileUploadClient():
                         pbar.update(len(chunk))
                     else:
                         log_error(f"Failed to Upload. {response.text}")
-                        break
+                        return False
         return True
     
     def iter_folder(self, path: Path):
+        success = True 
         if path.is_file():
-            self.upload_file(path)
+            success = success or self.upload_file(path)
         elif path.is_dir():
             for file in path.iterdir():
                 if file.is_file():
-                    self.upload_file(file)
-                # forbid any sub directory
+                    success = success or self.upload_file(file)
+        return success
                     
     def upload(self, file_path):
+        if not Path(file_path).exists():
+            log_error(f"Folder {file_path} does not exist.")
+            return
+        
         response = send_post_request("/task/client/add", params={"mname": self.mname, "tname": self.tname})
+        if response is None:
+            log_error("Cannot successfully get response. Please check your internet connection.")
+            return
         
         if response.status_code == 403:
             log_error(response.text)
             return
-        self.iter_folder(Path(file_path))
-        
-        send_post_request("/task/client/done", params={"mname": self.mname, "tname": self.tname})
-                    
-        print(f"Upload successfully. Visit xxx.com or use \
-            `powerinfer upload {self.mname+":"+self.tname} -s` to check the process.")
+        success = self.iter_folder(Path(file_path))
+        send_post_request("/task/client/done", params={"mname": self.mname, "tname": self.tname, "success": success})
+        if success:      
+            print(f"Upload successfully. Visit xxx.com or use `powerinfer upload {self.mname+":"+self.tname} -s` to check the process.")
+            print(f"Use `powerinfer upload {self.mname+":"+self.tname} -c` to cancel the training process.")
+        else:
+            log_error("Upload failed. Please rerun the command to resume uploading model.")
+            return
     
-    def fetch_history(self):
-        response = send_post_request("/task/client/query", params={"mname": self.mname, "tname": self.tname}, stream=True)
-        if response.status_code == 200:
-            try:
-                for line in response.iter_lines():
-                    if line:
-                        task = json.loads(line)
-                        yield task
-            except Exception as e:
-                log_error(f"Failed to parse task stream: {e}")
-                yield None
+    # def fetch_history(self):
+    #     response = send_post_request("/task/client/query", params={"mname": self.mname, "tname": self.tname}, stream=True)
+    #     if response.status_code == 200:
+    #         try:
+    #             for line in response.iter_lines():
+    #                 if line:
+    #                     task = json.loads(line)
+    #                     yield task
+    #         except Exception as e:
+    #             log_error(f"Failed to parse task stream: {e}")
+    #             yield None
         
     def cancel(self):
         print("Starting to cancel training task for model", self.mname + ":" + self.tname)
         response = send_post_request("/task/client/cancel", params={"mname": self.mname, "tname": self.tname})
+        if response is None:
+            log_error("Cannot successfully get response. Please check your internet connection.")
+            return
         if response.text == "true":
             print("Cancelled successfully.")
         else:
--- PIserver/commands/upload.py
+++ PIserver/commands/upload.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from PIserver.clients.FileUploadClient import FileUploadClient
 from PIserver.clients.net import send_post_request
 from PIserver.commands.command import Command
@@ -19,12 +20,20 @@ class Upload_Model(Command):
                 "mname": str(args.model).split(":")[0],
                 "tname": str(args.model).split(":")[1]
             })
+            if response is None:
+                log_error("Cannot successfully get response. Please check your internet connection.")
+                return
             if response.status_code == 404:
                 log_error(f"Model {args.model} not found.")
                 return
             task = dict(response.json())
             # {'tid': 'e8ec8383da200308f2f83fbfdf57589a', 'dir': 'D://train/73027646d141476489fd6dae05bdb217/testLlama-7b', 'version': 'd83d625b0d7683711c00bbd035eed394ea141609', 'id': 'c0ecd6558ad2e0838663e8b7b2c9bf9e', 'state': 'UPLOADING', 'created': '2025-03-27T20:56:30.123858', 'started': None, 'finished': None, 'queue': 0, 'progress': 0, 'waitingTime': 0, 'runningTime': 0}
-            print(task) # TODO:
+            print("STATE: ", task["state"])
+            print("version: ", task["version"])
+            print("Task created at: ", datetime.fromisoformat(task["created"]))
+            print("Task started at: ", datetime.fromisoformat(task["started"]) if task["started"] is not None else "--")
+            print("Task finished at: ", datetime.fromisoformat(task["finished"]) if task["finished"] is not None else "--")
+            
             return
         
         client = FileUploadClient(str(args.model).split(":"))
@@ -39,8 +48,6 @@ class Upload_Model(Command):
             print("Uploading model... Press Ctrl+C to cancel task.")
             
             client.upload(args.local_dir)
-            
-            print(f"Use `powerinfer upload {args.model} -c` to cancel the training process.")
              
         except KeyboardInterrupt:
             client.cancel()
